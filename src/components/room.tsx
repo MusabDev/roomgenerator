da4a31e0ae02a5f8aae47d8d5575ac79c24f60f6
Muhammad Musab <musabwebdev@gmail.com>
2023-09-12 16:43:12 +0600

    fix: download button spaces

--- src/components/room.tsx
+++ src/components/room.tsx
@@ -14,7 +14,7 @@ export function Room({ image, displayPrompt, room }: Doc<'rooms'>) {
           <Image src={image} alt="Living room" fill={true} />
 
           <div className="invisible absolute bottom-0 left-0 w-full scale-95 p-2 opacity-0 transition-all duration-300 group-hover:visible group-hover:scale-100 group-hover:opacity-100">
-            <div className="mt-2 w-full rounded-md bg-background/80 px-4 py-2 text-sm font-medium backdrop-blur-md">
+            <div className="mt-2 flex w-full flex-wrap items-center gap-2 rounded-md bg-background/80 px-4 py-2 text-sm font-medium backdrop-blur-md">
               Prompt: {displayPrompt}
               <Link
                 href={image}
@@ -22,7 +22,7 @@ export function Room({ image, displayPrompt, room }: Doc<'rooms'>) {
                   buttonVariants({
                     size: 'sm',
                   }),
-                  'ml-3 h-auto px-2 py-1',
+                  'h-auto px-2 py-1',
                 )}
                 download={room}
               >
