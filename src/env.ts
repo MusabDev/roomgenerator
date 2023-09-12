bd79ed3885e6f0d2cbe8434e2eebfad521222dbe
Muhammad Musab <musabwebdev@gmail.com>
2023-09-12 15:42:53 +0600

    chore: update env.ts

--- src/env.ts
+++ src/env.ts
@@ -3,13 +3,15 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
-    CONVEX_DEPLOYMENT: z.string().url(),
+    CONVEX_DEPLOYMENT: z.string(),
+    CONVEX_DEPLOY_KEY: z.string().optional(),
   },
   client: {
-    NEXT_PUBLIC_CONVEX_URL: z.string().min(1),
+    NEXT_PUBLIC_CONVEX_URL: z.string().url().min(1),
   },
   runtimeEnv: {
     CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
+    CONVEX_DEPLOY_KEY: process.env.CONVEX_DEPLOY_KEY,
     NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
   },
 })
