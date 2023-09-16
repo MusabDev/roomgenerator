c83f3c1861327e0f3f67e5f292406b7673eeb942
Muhammad Musab <musabwebdev@gmail.com>
2023-09-16 07:40:24 +0600

    chore: update github link

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: 'Room Generator',
   website: 'https://musabdev.com',
-  github: 'https://github.com/musabdev',
+  github: 'https://github.com/musabdev/roomgenerator',
   twitter: 'https://twitter.com/musabdev',
 }
