487c7ef5291488d5fe11724637099bafeabb5f27
Muhammad Musab <musabwebdev@gmail.com>
2023-09-16 11:23:45 +0600

    chore: make the prompt text left aligned room component

--- src/components/room.tsx
+++ src/components/room.tsx
@@ -15,7 +15,7 @@ export function Room({ image, displayPrompt, room }: Doc<'rooms'>) {
           <Image src={image} alt="Living room" fill={true} />
 
           <div className="invisible absolute bottom-0 left-0 w-full scale-95 p-2 opacity-0 transition-all duration-300 group-hover:visible group-hover:scale-100 group-hover:opacity-100">
-            <div className="mt-2 flex w-full flex-wrap items-center gap-2 rounded-md bg-background/80 px-4 py-2 text-sm font-medium backdrop-blur-md">
+            <div className="mt-2 flex w-full flex-wrap items-center gap-2 rounded-md bg-background/80 px-4 py-2 text-left text-sm font-medium backdrop-blur-md">
               Prompt: {displayPrompt}
               <Link
                 href={image}
@@ -34,7 +34,7 @@ export function Room({ image, displayPrompt, room }: Doc<'rooms'>) {
         </>
       ) : (
         <div className="absolute bottom-0 left-0 w-full p-2">
-          <div className="mt-2 flex w-full flex-wrap items-center gap-2 rounded-md bg-background/80 px-4 py-2 text-sm font-medium backdrop-blur-md">
+          <div className="mt-2 flex w-full flex-wrap items-center gap-2 rounded-md bg-background/80 px-4 py-2 text-left text-sm font-medium backdrop-blur-md">
             Prompt: {displayPrompt}
           </div>
         </div>
